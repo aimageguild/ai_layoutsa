a82a3378474fbea3535f3e3c1df0086f20e88ad0
LatticeMage <LatticeMage@gmail.com>
2024-01-20 09:33:05 +0800

    upload

--- _includes/reveal.js
+++
@@ -1,7 +0,0 @@
-Reveal.initialize({
-    controls: true,
-    progress: true,
-    center: true,
-    hash: true,
-    // add any other configuration options you want
-});
--- _layouts.md
+++
@@ -1,5 +0,0 @@
----
-layout: directory
----
-
-# _layouts
