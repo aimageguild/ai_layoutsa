57d189f083d234c108a4fc8ae12e98545c5941d7
AIMageGuild <AIMageGuild@users.noreply.github.com>
2024-02-02 12:35:15 +0800

    upload

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
-git config --local user.name "AI-Mage"
-git config --local user.email "ai@posetmage.com"
+git config --local user.name "AIMageGuild"
+git config --local user.email "AIMageGuild@users.noreply.github.com"
 git remote set-url origin git@AI:aimageguild/jekyll-shared-components.git
 
 git pull
